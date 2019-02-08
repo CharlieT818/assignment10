fbf8aba528eade6c32ae135ccaf3dfa6ff246d81
Marc Raphael <marc.raphael12@gmail.com>
2019-02-07 18:43:59 -0800

    finished

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ var index = require('./routes/index');
 var Math_Homework = require('./routes/Math_Homework');
 var AddActivity = require('./routes/AddActivity');
 var EditActivity = require('./routes/EditActivity');
+var Statistics = require('./routes/Statistics');
+var StatisticsMath = require('./routes/StatisticsMath');
+var StartReading = require('./routes/StartReading');
+var StartScience = require('./routes/StartScience');
+var StartCoding = require('./routes/StartCoding');
+var Settings = require('./routes/Settings');
+var SettingsMath = require('./routes/SettingsMath');
+var StartExercises = require('./routes/StartExercises');
+var BeginTimer = require('./routes/BeginTimer');
 // Example route
 // var user = require('./routes/user');
 
@@ -43,6 +52,15 @@ app.get('/index', index.view);
 app.get('/Math_Homework', Math_Homework.view);
 app.get('/AddActivity', AddActivity.view);
 app.get('/EditActivity', EditActivity.view);
+app.get('/Statistics', Statistics.view);
+app.get('/StatisticsMath', StatisticsMath.view);
+app.get('/StartReading', StartReading.view);
+app.get('/StartScience', StartScience.view);
+app.get('/StartCoding', StartCoding.view);
+app.get('/Settings', Settings.view);
+app.get('/SettingsMath', SettingsMath.view);
+app.get('/StartExercises', StartExercises.view);
+app.get('/BeginTimer', BeginTimer.view);
 // Example route
 // app.get('/users', user.list);
 
