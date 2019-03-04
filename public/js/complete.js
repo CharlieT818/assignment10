14c7b9ffc679f57aa2c61660e02a7c58197929d8
Marc Raphael <marc.raphael12@gmail.com>
2019-03-03 19:55:57 -0800

    exercises

--- public/js/complete.js
+++ public/js/complete.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
     $(".fab").addClass("expand");
     $(".fab-sha").addClass("fabactive");
     $(".close").addClass("active");
-    $("#text").html("Select an exercise: <p>Focus breathing</p> <p>Eyesight reset</p> <p>Simple hand exercise</p> <p>T-pose head roll</p>");
+    delay(function(){
+    $("#text").html("Choose an exercise");
+    $("#exercise").css("display", "block");
+    },400);
     });
 
     $(".close").on('click', function(){
@@ -12,9 +15,56 @@ $(document).ready(function() {
     $(".fab").removeClass("expand");
     $("#text").html("");
     $(".fab-sha").removeClass("fabactive");
+    $("#exercise").css("display", "none");
+    $("#textinstructions").css("display", "none");
     delay(function(){
     $("#spa").css("color", "#FFF");
     },800);
+    });
+
+    $(".button_resume").on('click', function(){
+    $(".close").removeClass("active");
+    $(".fab").removeClass("expand");
+    $("#text").html("");
+    $(".fab-sha").removeClass("fabactive");
+    $("#exercise").css("display", "none");
+    $(".button_resume").css("display", "none");
+    $("#textinstructions").css("display", "none");
+    delay(function(){
+    $("#spa").css("color", "#FFF");
+    },800);
+    });
+
+    $("#focus").click(function(){
+        $("#text").html("Focus Breathing");
+        $("#exercise").css("display", "none");
+        $("#textinstructions").css("display", "block");
+        $(".button_resume").css("display", "block");
+        $("#textinstructions").html("<ul><li>Think of a word or phrase to help you feel relaxed</li><li>Close your eyes</li><li>Take a few big deep breaths</li><li>As you breathe picture that the air is filled with a sense of peace and transquility</li><li>As you breathe out imagine that your stress leaves with the air</li><li>Repeat several times</li></ul>");
+    });
+
+    $("#eye").click(function(){
+        $("#text").html("The Eye Reset");
+        $("#exercise").css("display", "none");
+        $("#textinstructions").css("display", "block");
+        $(".button_resume").css("display", "block");
+        $("#textinstructions").html("<ul><li>Remove all eyewear</li><li>Close your eyes</li><li>While cusping hands cover the eyes without pressing hands into eyes</li><li>Adjust hands as needed to block all light out</li><li>Hold for 30 seconds at a time for a total of 2 minutes</li></ul>");
+    });
+
+            $("#pose").click(function(){
+        $("#text").html("T-Pose Head Roll");
+        $("#exercise").css("display", "none");
+        $("#textinstructions").css("display", "block");
+        $(".button_resume").css("display", "block");
+        $("#textinstructions").html("<ul><li>Stand and spread legs half a step apart</li><li>Extend arms horizontally away from the body</li><li>Look down without folding the body</li><li>Roll the head counter-clockwise for 4 complete cycles</li><li>Reverse and roll the head clockwise for 4 more cycles</li><li>Repeat 3 times</li></ul>");
+    });
+
+                $("#square").click(function(){
+        $("#text").html("Square Breathing");
+        $("#exercise").css("display", "none");
+        $("#textinstructions").css("display", "block");
+        $(".button_resume").css("display", "block");
+        $("#textinstructions").html("<ul><li>Inhale deeply through the nose for 4 counts (count to 4)</li><li>Hold breath in for 4 counts</li><li>Exhale for 4 counts</li><li>Repeat 10 times </li></ul>");
     });
 });
 
