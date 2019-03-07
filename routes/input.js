575498683bccb67b9e9c54977b537468f7f6d0d3
Marc Raphael <marc.raphael12@gmail.com>
2019-03-06 22:33:13 -0800

    yes

--- routes/input.js
+++ routes/input.js
@@ -11,7 +11,5 @@ exports.inputactivities = function(request, response){
 	data.activities.push(json);
 	json = {'ActivityName': request.query.ActivityName3,'workLength':25, 'breakLength':5};
 	data.activities.push(json);
-	json = {'ActivityName': request.query.ActivityName4,'workLength':25, 'breakLength':5};
-	data.activities.push(json);
   response.render('activities', data);
 };
